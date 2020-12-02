d15668e57beb9f24a85c8ded26f9d69401327c26
reach2sayan <sayan_samanta@brown.edu>
2020-12-02 15:04:47 -0500

    [FIX] Pandas filters -> MongoDB queries

--- app_files/dashapp/dashboard.py
+++ app_files/dashapp/dashboard.py
@@ -17,7 +17,7 @@ from plotly.subplots import make_subplots
 
 from .layout import html_layout
 
-def init_callbacks(app,df_ebal,df_unfcc,palette):
+def init_callbacks(app):
 
 # Dropdowns - World map
     @app.callback(
@@ -27,10 +27,15 @@ def init_callbacks(app,df_ebal,df_unfcc,palette):
     )
     def plot_world_map(trans, comm):
 
-        plot_df = df_ebal.query("(TRANSACTION == @trans) and (COMMODITY == @comm)")
+#        plot_df = df_ebal.query("(TRANSACTION == @trans) and (COMMODITY == @comm)")
+        plot_df = pd.DataFrame(col_ebal.aggregate([{'$match': {'TRANSACTION': trans, 'COMMODITY': comm}},
+                                                   { "$project": {"_id": 0 }},
+                                                   {'$group': {'_id': '$REF_AREA', 'avg_value': {'$avg': '$value'}}}]
+                                                 )
+                              ).rename(columns={"_id": "REF_AREA"}).set_index('REF_AREA').iloc[:,0]
         if plot_df.shape[0] == 0:
             return
-        plot_df = plot_df.groupby(["REF_AREA"])['value'].mean()
+#        plot_df = plot_df.groupby(["REF_AREA"])['value'].mean()
         fig = go.Figure()
         fig.add_trace(
             go.Choropleth(
@@ -80,12 +85,14 @@ def init_callbacks(app,df_ebal,df_unfcc,palette):
         else:
             country = country['points'][0]['location']
 
-        plot_df_ebal = df_ebal.query("(TRANSACTION == @trans) and (COMMODITY == @comm) and (REF_AREA == @country)") 
-        
-        if country in df_unfcc_co2['REF_AREA'].unique():
+        #plot_df_ebal = df_ebal.query("(TRANSACTION == @trans) and (COMMODITY == @comm) and (REF_AREA == @country)") 
+        plot_df_ebal = pd.DataFrame(col_ebal.find({'TRANSACTION':trans, 'COMMODITY':comm, 'REF_AREA':country}))
+        if country in col_unfcc.distinct('REF_AREA'):
             fig = go.Figure()
             fig = make_subplots(specs=[[{"secondary_y": True}]])
-            plot_df_unfcc = df_unfcc_co2.query("(REF_AREA == @country)")
+            plot_df_unfcc = pd.DataFrame(col_unfcc.find({'INDICATOR':'EN_ATM_CO2E_XLULUCF',
+                                                         'REF_AREA':country})
+                                        )
             
             fig.add_trace(
                 go.Bar(
@@ -181,15 +188,31 @@ def init_callbacks(app,df_ebal,df_unfcc,palette):
             year = 2012
         else:
             year = year['points'][0]['x']
+
         if country is None:
             country = 'USA'
         else:
             country = country['points'][0]['location']
         
-        plot_df_pie = df_ebal.query("(TRANSACTION == @transactions) and (COMMODITY == @comm)").reset_index(drop=True)
-        plot_df_pie = plot_df_pie.query("(TIME_PERIOD in '{}') and (REF_AREA in '{}')".format(str(year),country))
-        plot_df_bar = df_ebal.query("(TRANSACTION == @trans) and (COMMODITY == @commodities)").reset_index(drop=True)
-        plot_df_bar = plot_df_bar.query("(TIME_PERIOD in '{}') and (REF_AREA in '{}')".format(str(year),country))
+        plot_df_pie = pd.DataFrame(col_ebal.find({'$and': [{'TRANSACTION': { '$in': transactions}}, 
+                                                           {'COMMODITY': comm},
+                                                           {'TIME_PERIOD': year},
+                                                           {'REF_AREA':'{}'.format(country)}
+                                                          ]
+                                                 })
+                                  )
+        plot_df_bar = pd.DataFrame(col_ebal.find({'$and': [{'TRANSACTION': trans}, 
+                                                           {'COMMODITY': { '$in': commodities}},
+                                                           {'TIME_PERIOD': year},
+                                                           {'REF_AREA':'{}'.format(country)}
+                                                          ]
+                                                 })
+                                  )
+
+        #plot_df_pie = df_ebal.query("(TRANSACTION == @transactions) and (COMMODITY == @comm)").reset_index(drop=True)
+        #plot_df_pie = plot_df_pie.query("(TIME_PERIOD in '{}') and (REF_AREA in '{}')".format(str(year),country))
+        #plot_df_bar = df_ebal.query("(TRANSACTION == @trans) and (COMMODITY == @commodities)").reset_index(drop=True)
+        #plot_df_bar = plot_df_bar.query("(TIME_PERIOD in '{}') and (REF_AREA in '{}')".format(str(year),country))
         
 
         bar_fig = go.Figure()
@@ -225,10 +248,11 @@ def init_callbacks(app,df_ebal,df_unfcc,palette):
 
 def init_dashboard(server):
 
-    #MONGO_URI = 'mongodb+srv://sayan:infinity@infinity.9hew3.mongodb.net/<dbname>?retryWrites=true&w=majority'
-    MONGO_URI='localhost:27017'
+    MONGO_URI = 'mongodb+srv://sayan:infinity@infinity.9hew3.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    #MONGO_URI='localhost:27017'
     client = pymongo.MongoClient(MONGO_URI)
 
+    global col_ebal, col_unfcc
     db = client.UNSD
     col_ebal = db.ebal
     col_unfcc = db.unfcc
@@ -236,10 +260,10 @@ def init_dashboard(server):
     global commodities
     global transactions
     global palette
-    global df_unfcc
-    global df_ebal
-    global df_unfcc_co2
-    global df_inner
+    #global df_unfcc
+    #global df_ebal
+    #global df_unfcc_co2
+    #global df_inner
 
     commodities = [
         'Oil Products',
@@ -262,9 +286,9 @@ def init_dashboard(server):
         'Electricity, Heat and CHP plants',
     ]
 
-    df_unfcc = pd.DataFrame(col_unfcc.find()).drop("_id", axis=1)
-    df_ebal = pd.DataFrame(col_ebal.find()).drop("_id", axis=1)
-    df_ebal_small = df_ebal.query("(COMMODITY in @commodities) and (TRANSACTION in @transactions)").reset_index(drop=True)
+    #df_unfcc = pd.DataFrame(col_unfcc.find()).drop("_id", axis=1)
+    #df_ebal = pd.DataFrame(col_ebal.find()).drop("_id", axis=1)
+    #df_ebal_small = df_ebal.query("(COMMODITY in @commodities) and (TRANSACTION in @transactions)").reset_index(drop=True)
     palette = {
         'background': '#fcfaf2',
         'text': '#787c7a',
@@ -272,8 +296,8 @@ def init_dashboard(server):
         'lake': '#7bb2d4',
         'land': '#d4c981'
     }
-    df_unfcc_co2 = df_unfcc[df_unfcc['INDICATOR'] == 'EN_ATM_CO2E_XLULUCF']
-    df_inner = pd.merge(df_unfcc_co2, df_ebal_small, on=['REF_AREA','TIME_PERIOD'], how='inner')
+    #df_unfcc_co2 = df_unfcc[df_unfcc['INDICATOR'] == 'EN_ATM_CO2E_XLULUCF']
+    #df_inner = pd.merge(df_unfcc_co2, df_ebal_small, on=['REF_AREA','TIME_PERIOD'], how='inner')
 
     blank_fig = go.Figure()
     blank_fig.add_trace(go.Choropleth())
@@ -386,7 +410,7 @@ def init_dashboard(server):
             'backgroundColor': palette['background']
         }
     )
-    init_callbacks(app,df_ebal,df_unfcc,palette)
+    init_callbacks(app)
 
     return app.server
     
